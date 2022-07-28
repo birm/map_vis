44bb9b569481d8f97d54375c75856fdf49d8d3d2
Ryan Birmingham <birm@rbirm.us>
2022-07-28 07:00:37 -0400

    select a field

--- heatmap.js
+++ heatmap.js
@@ -18,25 +18,56 @@ var cfg = {
   // which field name in your data represents the longitude - default "lng"
   lngField: 'Stop_Lon',
   // which field name in your data represents the data value - default "value"
-  valueField: 'Total_Weekly_Avg_Pre_COVID_Boardings'
+  valueField: '_show_data'
 };
 
 var heatmapLayer = new HeatmapOverlay(cfg);
 
-d3.csv("data.csv").then(data=>{
-  console.log(data.length)
-  var baseLayer = L.tileLayer(
-    'http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',{
-      attribution: 'OSM',
-      maxZoom: 18
+var baseLayer = L.tileLayer(
+  'http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',{
+    attribution: 'OSM',
+    maxZoom: 18
+  }
+);
+
+var map = new L.Map('map', {
+  center: new L.LatLng(33.8366054, -84.3584737),
+  zoom: 11,
+  layers: [baseLayer, heatmapLayer]
+});
+
+var mapData = []
+
+
+function changeData(){
+  // get which field to show
+  field = document.getElementById("field_selector").value
+  for (let i=0; i<mapData.length; i++){
+    let v = mapData[i][field]
+    if (v == "No"){
+      v = 0
     }
-  );
+    else if (v== "Yes"){
+      v = 100
+    }
+    mapData[i]['_show_data'] = v
+  }
+  heatmapLayer.setData({data:mapData});
+}
 
-  var map = new L.Map('map', {
-    center: new L.LatLng(33.8366054, -84.3584737),
-    zoom: 11,
-    layers: [baseLayer, heatmapLayer]
-  });
-
-  heatmapLayer.setData({data:data});
+// data preproc
+d3.csv("data.csv").then(data=>{
+  mapData = data
+  let selector = document.getElementById("field_selector")
+  // !may be issue if user changes selector before this finishes
+  selector.onchange = changeData;
+  // populate the selector
+  for(let i of Object.keys(mapData[0])){
+    let opt = document.createElement("option")
+    opt.value = i
+    opt.innerHTML = i
+    selector.appendChild(opt)
+  }
+  // handle data
+  changeData()
 });
